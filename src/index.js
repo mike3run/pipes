4568ad24ea4d61ee3e5e032fe53647362b126323
Miguel Palau <mpalauzarza@gmail.com>
2018-01-15 18:17:59 -0600

    gulp pug as well

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import critical from './pipes/critical'
+import html from './pipes/html'
 import minifyStyles from './pipes/minifyStyles'
+import pug from './pipes/pug'
 import purify from './pipes/purify'
 import styles, {getJSON} from './pipes/styles'
 
 export {
   critical,
+  html,
   minifyStyles,
+  pug,
   purify,
   styles,
   getJSON
